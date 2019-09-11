507a7801cb896ff5fb5ca8e6819e5b7f661eb77b
Mahtab Ameli <mahtab.ameli@gmail.com>
2019-09-11 16:51:09 -0400

    Description of the email table

---
+++ mailcleaner/db/models/Email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from mailcleaner.db import base
+from mailcleaner.db.helpers import MCBooleanStringEnum
+from sqlalchemy import Column, String, Enum
+from sqlalchemy.dialects.mysql import INTEGER
+from . import BaseModel
+
+
+class Administrator(base, BaseModel):
+    """
+    administrator table
+    """
+    __tablename__ = 'administrator'
+
+    address = Column(String(120), nullable=False)
+    user = Column(INTEGER(11), nullable=False, default=0)
+    is_main = Column(Enum(MCBooleanStringEnum), nullable=False, default=MCBooleanStringEnum.true.value)
+    pref = Column(INTEGER(11), nullable=False, default=1)
+    id = Column(INTEGER(11), nullable=False, primary_key=True)
