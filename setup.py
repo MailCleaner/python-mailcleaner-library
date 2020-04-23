0edbe285054e2239d71c6d489563852d820246d6
Vincent Lauria <vincent@lauria.ch>
2020-04-23 12:48:02 +0200

    🚧Switch to Python 3.7.7

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup(name='mailcleaner-library',
-      version='1.0',
-      description='MailCleaner Python Library',
-      url='https://github.com/MailCleaner/python-mailcleaner-library',
-      author='Mentor Reka',
-      author_email='reka@mailcleaner.net',
-      license='GPL',
-      packages=find_packages(),
-      install_requires=[
-          'Jinja2>=2.10,<2.11', 'SQLAlchemy>=1.3.7,<1.4', 'PyMySQL>=0.9,<0.10',
-          'factory-boy>=2.12,<2.13', 'netifaces>=0.10.9,<0.11',
-          'invoke>=1.4.1,<1.5'
-      ],
-      zip_safe=False)
+setup(
+    name='mailcleaner-library',
+    version='1.0',
+    description='MailCleaner Python Library',
+    url='https://github.com/MailCleaner/python-mailcleaner-library',
+    author='Mentor Reka',
+    author_email='reka@mailcleaner.net',
+    license='GPL',
+    packages=find_packages(),
+    install_requires=[
+        'Jinja2>=2.10,<2.11',
+        'SQLAlchemy>=1.3.7,<1.4',
+        'PyMySQL>=0.9,<0.10',
+        'factory-boy>=2.12,<2.13',
+        'netifaces>=0.10.9,<0.11',
+        'invoke>=1.4.1,<1.5',
+    ],
+    python_requires='>=3.7.7',
+    zip_safe=False)
