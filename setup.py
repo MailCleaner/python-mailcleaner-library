355d0ce7d053892c68513b4a7954aa3ff7541f11
Daniel Jorge <jorge.daniel@protonmail.com>
2020-04-25 22:14:34 +0200

    :pushpin: Updating dependencies

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ setup(
     license='GPL',
     packages=find_packages(),
     install_requires=[
-        'Jinja2>=2.11.2,<2.11.2',
-        'SQLAlchemy>=1.3.16,<1.3.16',
-        'PyMySQL>=0.9.3,<0.9.3',
-        'factory-boy>=2.12.0,<2.12.0',
-        'netifaces>=0.10.9,<0.10.9',
-        'invoke>=1.4.1,<1.4.1',
-        'click>=7.1.1,<7.1.1',
-        'requests>=2.23.0,<2.23.0'
+        'Jinja2>=2.11.2,<=2.11.2',
+        'SQLAlchemy>=1.3.16,<=1.3.16',
+        'PyMySQL>=0.9.3,<=0.9.3',
+        'factory-boy>=2.12.0,<=2.12.0',
+        'netifaces>=0.10.9,<=0.10.9',
+        'invoke>=1.4.1,<=1.4.1',
+        'click>=7.1.1,<=7.1.1',
+        'requests>=2.23.0,<=2.23.0'
     ],
     scripts=['bin/fail2ban.py'],
     python_requires='>=3.7.7',
